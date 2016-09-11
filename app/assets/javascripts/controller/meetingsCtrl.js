8291e32641de702a5c0caa927523b296969f1197
Tom Yang <xy2286@columbia.edu>
2016-09-11 11:08:17 -0400

    angular and CRUD stuff

---
+++ app/assets/javascripts/controller/meetingsCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+   'use strict';
+
+     angular.module("app").controller('testCtrl', function($scope){
+     $scope.message = "feed the cat !";
+
+   });
+})();
--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,6 +8,65 @@ class MeetingsController < ApplicationController
     render 'index.html.erb'
   end
 
+  def new
+    @meeting = Meeting.new(start_time: DateTime.now, end_time: 1.hour.from_now)
+    @tags = Tag.all
+
+  end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes]
+    )
+    if @meeting.save
+      params[:tag_ids].each do |tag_id|
+        MeetingTag.create(meeting_id: @meeting.id, tag_id: tag_id)
+      end
+      flash[:success] = "Meeting was successfully saved !"
+      redirect_to "/meetings"
+    else
+      @tags = Tag.all
+      flash[:error] = "Hey! Wrong !"
+      # flash[:danger] = @meeting.errors.full_message
+      render "new"
+    end
+  end
+
+  def edit
+    @meeting = Meeting.find_by(id: params[:id])
+
+
+  end
+
+  def update
+    @meeting = Meeting.find_by(id: params[:id])
+    if @meeting.update(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes]
+    )
+      @meeting.tags.destroy_all
+      params[:tag_ids].each do |tag_id|
+        MeetingTag.create(meeting_id: @meeting.id, tag_id: tag_id)
+      end
+      flash[:success] = "yes"
+      redirect_to "/meetings"
+    else
+      flash[:error] = "no"
+      redirect_to "/meetings/#{@meeting.id}/edit"
+    end
+  end
+
+  def destroy
+
+  end
+
   def show
     @meeting = Meeting.find_by(id: params[:id])
     render 'show.html.erb'
